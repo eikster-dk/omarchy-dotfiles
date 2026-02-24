9fa15c1cbd81faf6db7a9e541fd9941e6c864783
Eik Emil Bengtsson <git.noreply@e16n.dev>
2026-02-24 18:01:11 +0100

    feat: web configuration for my ZSA voyager

--- omarchy-webapps.sh
+++ omarchy-webapps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-omarchy-webapp-install SimpleLogin  "https://app.simplelogin.io/dashboard/" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/simplelogin.png"
-omarchy-webapp-install DashboardIcons  "https://dashboardicons.com/icons" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/dashboard-icons-dark.png"
+omarchy-webapp-install SimpleLogin "https://app.simplelogin.io/dashboard/" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/simplelogin.png"
+omarchy-webapp-install DashboardIcons "https://dashboardicons.com/icons" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/dashboard-icons-dark.png"
 omarchy-webapp-install Claude "https://claude.ai/new" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/claude-ai.png"
+
+omarchy-webapp-install "ZSA Voyager" "https://configure.zsa.io/voyager" "https://configure.zsa.io/assets/oryx-logo-navbar-LAgSxL-X.png"
